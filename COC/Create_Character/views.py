c50e40e5fc6bd1cac59bfc3441ddc735087723fd
Shuo-Ren, Lin <ntp890517.ee96@g2.nctu.edu.tw>
2018-02-04 16:47:48 +0800

    create a test page, handling views, urls and template

--- COC/Create_Character/views.py
+++ COC/Create_Character/views.py
@@ -1,3 +1,18 @@
+from django.http import Http404
 from django.shortcuts import render
+from Create_Character.models import Character
 
-# Create your views here.
+def test(request):
+    try:
+        c = Character.objects.all()[:1].get()
+    except Character.DoesNotExist():
+        raise Http404("Character does not exist")
+    return render(request, 'test.html', {'character': c})
+#from polls.models import Poll
+
+#def test(request, poll_id):
+#    try:
+#        p = Poll.objects.get(pk=poll_id)
+#    except Poll.DoesNotExist:
+#        raise Http404("Poll does not exist")
+#    return render(request, 'polls/detail.html', {'poll': p})
