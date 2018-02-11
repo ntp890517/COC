16b57e804ba03bcca0302f2812621d6aaf65720a
Shuo-Ren, Lin <ntp890517.ee96@g2.nctu.edu.tw>
2018-02-11 11:30:30 +0800

    register models, Job, SkillCategory, Skill, WeaponCategory and Weapon

--- COC/Create_Character/admin.py
+++ COC/Create_Character/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from Create_Character.models import Character
+from Create_Character.models import Job
+from Create_Character.models import SkillCategory, Skill
+from Create_Character.models import WeaponCategory, Weapon
 
 # Register your models here.
 admin.site.register(Character)
+admin.site.register(Job)
+admin.site.register(SkillCategory)
+admin.site.register(Skill)
+admin.site.register(WeaponCategory)
+admin.site.register(Weapon)
