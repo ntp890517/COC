1b3808c33f2f19818c005d87c9bd6c8bb857f601
Shuo-Ren, Lin <ntp890517.ee96@g2.nctu.edu.tw>
2018-03-17 16:02:27 +0800

    tweak

--- COC/static/javascript/COC.js
+++ COC/static/javascript/COC.js
@@ -1,22 +1,13 @@
 function openCatalog(evt, catalogName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-
     // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+    $(".tabcontent").css("display", "none");
 
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    $(".tablinks").removeClass("active");
 
     // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(catalogName).style.display = "block";
-    evt.target.className += " active";
+    $("#" + catalogName).css("display", "block");
+    evt.target.addClass("active");
 }
 
 $( document ).ready(function() {
