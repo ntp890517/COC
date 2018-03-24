e2a2e0983c3955f9c1b83f6f35c6571516954201
hanky <hankyeh2025>
2018-03-24 15:08:21 +0800

    骰子功能新增

--- COC/static/javascript/COC.js
+++ COC/static/javascript/COC.js
@@ -10,6 +10,38 @@ function openCatalog(evt, catalogName) {
     $(evt.target).addClass("active");
 }
 
+function rollOneDice() {
+return Math.ceil(Math.random()*6)
+}
+
+function rollThreeDice() {
+return rollOneDice()+rollOneDice()+rollOneDice()
+}
+
+function rollTwoDicePlus6 () {
+return rollOneDice()+rollOneDice()+6
+}
+
+
 $( document ).ready(function() {
     $( "#btnCat1" ).click();
+    $(".label-dice").css("display", "none");
+    $("#roll-dice").click(
+    function() {
+    $("#D1-1").text((rollThreeDice()*5).toString()).css("display", "inline-block");
+    $("#D1-2").text((rollThreeDice()*5).toString()).css("display", "inline-block");
+    $("#D1-3").text((rollThreeDice()*5).toString()).css("display", "inline-block");
+    $("#D1-4").text((rollThreeDice()*5).toString()).css("display", "inline-block");
+    $("#D1-5").text((rollThreeDice()*5).toString()).css("display", "inline-block");
+    $("#D1-6").text((rollThreeDice()*5).toString()).css("display", "inline-block");
+    $("#D1-7").text((rollThreeDice()*5).toString()).css("display", "inline-block");
+    $("#D2-1").text((rollTwoDicePlus6()*5).toString()).css("display", "inline-block");
+    $("#D2-2").text((rollTwoDicePlus6()*5).toString()).css("display", "inline-block");
+    $("#D2-3").text((rollTwoDicePlus6()*5).toString()).css("display", "inline-block");
+    $("#D2-4").text((rollTwoDicePlus6()*5).toString()).css("display", "inline-block");
+    $("#D3-1").text((rollThreeDice()*5).toString()).css("display", "inline-block");
+    $("#D3-2").text((rollThreeDice()*5).toString()).css("display", "inline-block");
+    });
+
 });
+
